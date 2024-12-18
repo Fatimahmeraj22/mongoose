15a00ada0a621ab53f8937ca721b4933f3cc2916
Fatimahmeraj22 <merajfaate@gmail.com>
2024-12-18 05:49:53 +0000

    mongoose

---
+++ app.js
@@ -0,0 +1,15 @@
+import express from "express";
+const app = express;
+const PORT = 5000;
+
+aapp.get("/fatima",(req,res)=>{
+    try {
+       res.status (200).send("hello wolrddddddd")
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+app.listen(PORT ,() => {
+  console.log(`server is up and running ${PORT}`);
+});
